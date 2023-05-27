07197a42760288890a7adafddb1d695b059b833b
Nova Andre Saputra <novaandre.saputraadit@gmail.com>
2023-05-27 15:34:51 +0700

    ONG: remove emoticon 5

--- webhook.py
+++ webhook.py
@@ -1,15 +1,22 @@
-# -*- coding: utf-8 -*-
-from flask import Flask,render_template,url_for,request
+# from flask import Flask,render_template,url_for,request
+import urllib.parse
 import pickle
 import json
 import pandas as pd
 import requests
+import time
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/predict',methods=['POST'])
+# @app.route('/predict',methods=['POST'])
 def predict():
-	results = requests.get("https://api.telegram.org/bot6004748378:AAGjqym8DMEj60N3-f59NXIuQewty8rDbCs/getUpdates")
+	token = "6117835761:AAEGYEDHUjqfHj3tTPcq3VqdfS-2PTjmHbs"
+	file_offset = open('offset_update_id.txt', 'r+')
+	last_update_id = file_offset.read()
+	file_offset.close()
+	results = requests.get("https://api.telegram.org/bot"+token+"/getUpdates")
+	if last_update_id:
+		results = requests.get("https://api.telegram.org/bot"+token+"/getUpdates?offset="+last_update_id)
 	results = results.json()
 
 	prediction = pd.Series({'status': "fail", 'message': "Internal Server Erorr", 'data': {'output': []}})
@@ -25,22 +32,36 @@ def predict():
 		loaded_model = pickle.load(f2)
 		f2.close()
 
-		for result in results['result']:
-			chat_id_from = result['message']['chat']['id']
-			message = result['message']['text']
-			data = [message]
-			vect = loaded_vectorizer.transform(data)
-			output = loaded_model.predict(vect)
-			prediction.status = "ok"
-			prediction.message = "Data is retrieved successfuly"
+		for result in reversed(results['result']):
+			if str(result["update_id"]) != str(last_update_id):
+				file_offset = open('offset_update_id.txt', 'w')
+				file_offset.write(str(result["update_id"]))
+				file_offset.close()
 
-			if(int(output)):
-				# text_prediction = "Halo kak, untuk kenyamanan bersama mohon untuk tidak mengirimkan pesan yang menyimpang dari topik obrolan grup yaa. Yuk kirim lagi pesan yang berkaitan dengan lowongan pekerjaan! —JoobseekBot"
-				text_prediction = "hi"
-			requests.get("https://api.telegram.org/bot6004748378:AAGjqym8DMEj60N3-f59NXIuQewty8rDbCs/sendMessage?parse_mode=HTML&chat_id="+str(chat_id_from)+"&text="+text_prediction)
+				chat_id_from = result['message']['chat']['id']
+				message = result['message']['text']
+				data = [message]
+				vect = loaded_vectorizer.transform(data)
+				output = loaded_model.predict(vect)
+				prediction.status = "ok"
+				prediction.message = "Data is retrieved successfuly"
+
+				if(int(output)):
+					requests.get("https://api.telegram.org/bot"+token+"/deleteMessage?chat_id="+str(chat_id_from)+"&message_id="+str(result["message"]["message_id"]))
+					text_prediction = "Halo kak <b>"+result["message"]["from"]["first_name"]+"</b>, untuk kenyamanan bersama mohon untuk tidak mengirimkan pesan yang menyimpang dari topik obrolan grup yaa. Yuk kirim lagi pesan yang berkaitan dengan lowongan pekerjaan! 🙌\n—<i>JoobseekBot</i> ☺️"
+					response = requests.get("https://api.telegram.org/bot"+token+"/sendMessage?parse_mode=HTML&chat_id="+str(chat_id_from)+"&text="+urllib.parse.quote(text_prediction, safe=''))
+					response = response.json()
+					time.sleep(5)
+					requests.get("https://api.telegram.org/bot"+token+"/deleteMessage?chat_id="+str(response["result"]["chat"]["id"])+"&message_id="+str(response["result"]["message_id"]))
+			else:
+				exit()
 	except BaseException as e:
 		prediction.message = str(e)
+		print(e)
 		return prediction.to_json()
 if __name__ == '__main__':
-	app.run(host='0.0.0.0')
+	for i in range(3500):
+		predict()
+		time.sleep(1)
+	# app.run(debug=True)
 	
